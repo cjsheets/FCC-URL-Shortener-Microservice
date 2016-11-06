ae12494a90110bcf30999966bb0472d128d8c80c
Chad Sheets <cjsheets@gmail.com>
2016-11-06 23:52:43 +0000

    Added core logic

--- server.js
+++ server.js
@@ -1,20 +1,50 @@
-require('credential');
-var http = require("http");  
-const url = require('url');
-var mongodb = require('mongodb');
+var credential = require('./credential'),
+  http = require("http"),
+  validator = require('validator'),
+  url = require('url'),
+  mongodb = require('mongodb');
 
 var MongoClient = mongodb.MongoClient;
-var url = 'mongodb://' + credential + '@ds145997.mlab.com:45997/cjs-sandbox';      
+var dbCon = 'mongodb://' + credential.mlab + '@ds145997.mlab.com:45997/cjs-sandbox';      
 
 
 http.createServer(function(req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  var objUrl = url.parse(req.url, true),
-  console.log(objUrl);
+  var path = url.parse(req.url, true).path,
+    response = '';
+  if (/^\/new\//.test(path)) {
+    var URL = path.substr(5);
+    console.log(URL);
+    if( validator.isURL(URL) ){
+      console.log('add to database')
+    } else {
+      response = "{error: url was not valid}";
+    }
+  } else {
+    var URL = path.substr(1);
+    console.log(URL);
+    if( validator.isAlphanumeric(URL) ){
+      console.log('retrieve from database')
+    } else {
+      response = "{error: redirect is not valid}";
+    }
+
+  }
 }).listen(process.env.PORT || 8080);
 
-function checkDb(){
-  MongoClient.connect(url, function (err, db) {
+function storeInDb(){
+  MongoClient.connect(dbCon, function (err, db) {
+    if (err) {
+      console.log('Unable to connect to the mongoDB server. Error:', err);
+    } else {
+      console.log('Connection established to', url);
+      db.close();
+    }
+  });
+}
+
+function retrieveFromDb(){
+  MongoClient.connect(dbCon, function (err, db) {
     if (err) {
       console.log('Unable to connect to the mongoDB server. Error:', err);
     } else {
