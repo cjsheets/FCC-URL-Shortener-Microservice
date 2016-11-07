1e9ff3b36c04d6ca2be925b2e15d18c4ef13d46b
Chad Sheets <cjsheets@gmail.com>
2016-11-07 02:01:24 +0000

    Added basic db connectivity

--- Readme.md
+++ Readme.md
@@ -2,4 +2,6 @@
 
 This is a NodeJS service that produces shortened URLs.
 
+Requires credential.js to interface with mLab.
+
 [![Analytics](https://cjs-beacon.appspot.com/UA-10006093-3/github/cjsheets/FCC-URL-Shortener-Microservice?pixel)](https://github.com/cjsheets/FCC-URL-Shortener-Microservice)
--- server.js
+++ server.js
@@ -5,51 +5,67 @@ var credential = require('./credential'),
   mongodb = require('mongodb');
 
 var MongoClient = mongodb.MongoClient;
-var dbCon = 'mongodb://' + credential.mlab + '@ds145997.mlab.com:45997/cjs-sandbox';      
-
+var dbCon = 'mongodb://' + credential.mlab() + '@ds145997.mlab.com:45997/cjs-sandbox';      
 
 http.createServer(function(req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   var path = url.parse(req.url, true).path,
     response = '';
-  if (/^\/new\//.test(path)) {
-    var URL = path.substr(5);
-    console.log(URL);
-    if( validator.isURL(URL) ){
-      console.log('add to database')
-    } else {
-      response = "{error: url was not valid}";
-    }
-  } else {
-    var URL = path.substr(1);
-    console.log(URL);
-    if( validator.isAlphanumeric(URL) ){
-      console.log('retrieve from database')
-    } else {
-      response = "{error: redirect is not valid}";
-    }
-
-  }
-}).listen(process.env.PORT || 8080);
-
-function storeInDb(){
   MongoClient.connect(dbCon, function (err, db) {
     if (err) {
       console.log('Unable to connect to the mongoDB server. Error:', err);
+      response = "{error: service is temporarally unavailable}";
     } else {
+      if (/^\/new\//.test(path)) {
+        var URL = path.substr(5);
+        console.log('Add: ' + URL);
+        if( validator.isURL(URL) ){
+          var insertString = [{shorturl: genURL()}];
+          console.log('Mongo: ' + insertString)
+          insertDocument(db, insertString, function(result){
+            console.log('callback: ' + result);
+          })
+        } else {
+          response = "{error: url was not valid}";
+        }
+      } else {
+        var URL = path.substr(1);
+        console.log('Check: ' + URL);
+        if( validator.isAlphanumeric(URL) ){
+          console.log('retrieve from database')
+        } else {
+          response = "{error: redirect is not valid}";
+        }
+    
+      }
       console.log('Connection established to', url);
       db.close();
     }
+  });
+}).listen(process.env.PORT || 8080);
+
+var findDocument = function(db, str, callback) {
+  var collection = db.collection('documents');
+  collection.findOne(str, function(err, result) {
+    (err != null) ? callback(err) : callback(result);
   });
 }
 
-function retrieveFromDb(){
-  MongoClient.connect(dbCon, function (err, db) {
-    if (err) {
-      console.log('Unable to connect to the mongoDB server. Error:', err);
-    } else {
-      console.log('Connection established to', url);
-      db.close();
-    }
+var insertDocument = function(db, str, callback) {
+  var collection = db.collection('documents');
+  collection.insertMany(str, function(err, result) {
+    (err != null) ? callback(err) : callback(result);
   });
+}
+
+var genURL = function() {
+  var url = Math.random().toString(36).slice(2,6);
+  // findDocument(db, url, function(result){
+  //   if(result == '') {
+  //     return url;
+  //   } else {
+  //     return genURL();
+  //   }
+  // });
+  return url;
 }
